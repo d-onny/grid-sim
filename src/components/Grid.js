69c4a490a01169a942cf7144f0992a5c9e50887f
donny <donny.l5606@gmail.com>
2021-08-02 23:42:29 -0400

    Grid view of 7x6 done

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,20 +1,49 @@
 import React from "react";
 import Tile from "./Tile";
 
-const Grid = props => {
-    const rowCount = 7;
-    const columnCount = 6;
+class Grid extends React.Component {
 
-    // fillGrid = ()=>{
-        
-    // }
+    constructor(props){
+        super(props);
 
-    return (
-        <div className="grid-container">
-            <Tile/>
-            <Tile/>
-        </div>
-    )
+        // this.state = {
+        //     isSelected:false,
+        // };
+    }
+
+
+
+
+    renderGrid(column, row) {
+        //TODO - change to map
+        let gridMatrix = [];
+        for (let y = row-1; y > -1; y--){
+            let rowTiles = [];
+            for (let x = 0; x < column; x++){
+                rowTiles.push(<Tile key={{x}+{y}} x={x} y={y}
+                    />)
+            }
+            gridMatrix.push(<div>{rowTiles}</div>)
+        }
+
+        return(
+            <div className="grid">
+                {gridMatrix}
+            </div>
+        )
+    };
+
+    render(){
+        const rowCount = 7;
+        const columnCount = 6;
+        const grid = this.renderGrid(columnCount, rowCount)
+        return (
+            <div className="grid-container">
+                {grid}
+            </div>
+        )
+    }
+
 };
 
 export default Grid;
--- src/components/Tile.js
+++ src/components/Tile.js
@@ -4,11 +4,19 @@ import "./Tile.css"
 
 class Tile extends React.Component{
 
-    // let steps = props.steps;
+    constructor(props){
+        super(props);
+        let x = this.props.x;
+        let y = this.props.y;
+        this.state = {
+            isSelected:false,
+        };
+    }
 
     render(){
         return (
             <div className="square">
+                {this.props.x} {this.props.y}
                 {/* <button>
                 </button> */}
             </div>
